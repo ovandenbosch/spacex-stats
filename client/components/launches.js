a8bf084a0677bf0b0b9bd33ced4c7980d6597bdf
Oliver van den Bosch <vanders06@icloud.com>
2021-05-05 20:12:13 +0100

    Add launch query

--- client/components/launches.js
+++ client/components/launches.js
@@ -1,4 +1,6 @@
 import { useQuery, gql } from "@apollo/client";
+import LaunchItem from './LaunchItem'
+import MissionKey from './MissionKey'
 
 const LAUNCHES_QUERY = gql`
   query LaunchesQuery {
@@ -11,11 +13,25 @@ const LAUNCHES_QUERY = gql`
   }
 `;
 
+function LaunchQuery() {
+  const { loading, error, data } = useQuery(LAUNCHES_QUERY);
+
+  if (loading) return <h4>Loading...</h4>;
+  if (error) console.log(error);
+
+  return <>
+    {data.launches.map(launch => (
+      <LaunchItem key={launch.id} launch={launch} />
+    ))}
+  </>;
+}
+
 export default function Launches() {
   return (
-    <div>
+    <>
       <h1 className="display-4 my-3">Launches</h1>
-      
-    </div>
+      <MissionKey />
+      <LaunchQuery />
+    </>
   );
 }
