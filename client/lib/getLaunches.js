3b83ce9faed11377fe426d2053eb16157cdffb6d
Oliver van den Bosch <vanders06@icloud.com>
2021-05-07 22:50:11 +0100

    Remove unused functions

--- client/lib/getLaunches.js
+++ client/lib/getLaunches.js
@@ -1,45 +1,29 @@
-import client from '../apollo-client'
-import { gql } from '@apollo/client'
+import client from "../apollo-client";
+import { gql } from "@apollo/client";
 
-export async function getIds() {
-    const { data } = await client.query({
+export async function getLaunchData(id) {
+  const { data } = await client.query({
+    variables: {
+      id,
+    },
     query: gql`
-      query LaunchesQuery {
-        launches {
+      query LaunchQuery($id: Int!) {
+        launch(id: $id) {
+          mission_name
+          launch_year
+          launch_success
+          launch_date_local
           id
+          rocket {
+            rocket_name
+            rocket_type
+          }
         }
       }
     `,
   });
 
-  return data.launches.map((element) => {
-    
-    console.log({params: {id: element.id}})
-    return {
-      params: {
-        id: element.id
-      },
-      
-    }
-  })
-}
-
-export async function getLaunchData(id) {
-  const { data } = await client.query({
-    variables: {
-      id
-    },
-    query: gql`
-    query LaunchQuery($id: Int!) {
-      launch(id: $id) {
-        id
-      }
-    }
-    `
-  })
-
   return {
-    data
-  }
-  
+    data,
+  };
 }
